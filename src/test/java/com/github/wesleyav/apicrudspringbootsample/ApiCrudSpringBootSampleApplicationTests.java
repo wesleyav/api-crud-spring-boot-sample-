9831725c9a4177fd599bda0869ed6d59faf5376b
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-04-07 21:29:29 -0300

    Adiciona estrutura do projeto

---
+++ src/test/java/com/github/wesleyav/apicrudspringbootsample/ApiCrudSpringBootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.apicrudspringbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudSpringBootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

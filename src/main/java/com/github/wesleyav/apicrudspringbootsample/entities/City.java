8dfeca7eb90ca8d9548636f42d43433c52df9d8c
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-04-18 18:54:04 -0300

    Adiciona classe City

---
+++ src/main/java/com/github/wesleyav/apicrudspringbootsample/entities/City.java
@@ -0,0 +1,36 @@
+package com.github.wesleyav.apicrudspringbootsample.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "city")
+public class City implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "city")
+	private String name;
+
+	@Column(name = "last_update")
+	private Instant lastUpdate;
+
+}
